1dfae9a5a0940712229c82aa77a5baa70206e082
Jonah Brown <jswish02@gmail.com>
2019-09-15 02:08:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,1 +1,15 @@
-def hello_t
+def hello_t(array)
+  if black_given?
+    i=0 
+    while i < array.length
+    yeild(array[i])
+    i = i + 1 
+  end
+  
+  array 
+  
+else 
+  puts "hey! No block was given!"
+end
+end 
+
